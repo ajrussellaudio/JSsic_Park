cb20cdf15a0e72f90f03a2cd1834b4e190f6685a
Alan Russell <ajrussellaudio@gmail.com>
2018-06-06 09:56:56 +0100

    add acceptance criteria

---
+++ specs/Dinosaur_spec.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const Dinosaur = require("../Dinosaur");
+
+describe("Dinosaur", function() {
+  it("should have a type");
+
+  it("should have a number of offspring per year");
+})
