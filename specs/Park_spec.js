a85edc08de7e1307b0938724806e819bbf7b0f7e
Alan Russell <ajrussellaudio@gmail.com>
2018-06-06 10:07:54 +0100

    add park acceptance criteria

---
+++ specs/Park_spec.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const Park = require("../Park");
+
+describe("Park", function() {
+  it("enclosure should start empty");
+  it("should be able to add dinosaur");
+  it("should be able to remove all dinosaurs of a particular type");
+  it("should get all the dinosaurs with an offspring count of more than 2");
+});
